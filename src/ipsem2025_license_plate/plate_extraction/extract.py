189ae8969e6277485750942d278bc7aebe796d0f
Michal Klamecki <275486@student.pwr.edu.pl>
2025-03-26 21:46:52 +0100

    xyz

---
+++ src/ipsem2025_license_plate/plate_extraction/extract.py
@@ -0,0 +1,22 @@
+import torch
+import cv2 as cv
+import os
+from PIL import Image
+from yolov5 import YOLOv5
+
+def predict(input_folder, output_folder):
+    model = YOLOv5("best.pt")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for filename in os.listdir(input_folder):
+        image_path = os.path.join(input_folder, filename)
+        image = cv.imread(image_path)
+
+        if image is None:
+            print(f"Nie można wczytać obrazu: {filename}")
+            continue
+        output_path = os.path.join(output_folder, filename)
+        cv.imwrite(output_path, corrected_plate)
+        print(f"Przetworzono: {filename}, zapisano do {output_path}")
+
